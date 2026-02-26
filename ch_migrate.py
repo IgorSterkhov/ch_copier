688493806778fb27128a2ac7da79c25fdd185b7f
janis <jans.janis@hotmail.com>
2026-02-26 15:07:34 +0000

    fix new lines in tablesx ddl

--- ch_migrate.py
+++ ch_migrate.py
@@ -640,6 +640,8 @@ class CHMigrateApp:
                     ddl = self.source_client.command(
                         f"SHOW CREATE TABLE `{database}`.`{table}`"
                     )
+                if isinstance(ddl, str) and "\\n" in ddl:
+                    ddl = ddl.replace("\\n", "\n")
                 self.table_ddls[key] = ddl
             except Exception as e:
                 self.table_ddls[key] = f"-- Error: {e}"
